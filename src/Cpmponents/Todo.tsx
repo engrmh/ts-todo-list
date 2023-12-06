3e0f2bca34f6327e348ffdd76ed2154d53bae0e8
GALAHAD <59095451+engrmh@users.noreply.github.com>
2023-12-06 21:32:26 +0330

    feat: add font awesome to project

--- src/Cpmponents/Todo.tsx
+++ src/Cpmponents/Todo.tsx
@@ -1,21 +1,25 @@
 import React from "react";
 import { ITodo } from "./Todos.types";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
 
 interface ITodoPRops {
-  todo: ITodo
-  deleteTodo: (id: string) => void
-  toggleComplete: (id: string) => void
+  todo: ITodo;
+  deleteTodo: (id: string) => void;
+  toggleComplete: (id: string) => void;
 }
 
-function Todo({todo , deleteTodo , toggleComplete } : ITodoPRops) {
+function Todo({ todo, deleteTodo, toggleComplete }: ITodoPRops) {
   return (
     <div className="Todo">
       <p
-        className={`${todo.completed ? 'completed' : ''}`} // or completed className
+        className={`${todo.completed ? "completed" : ""}`} // or completed className
       >
-       {todo.title}
+        {todo.title}
       </p>
-      <div>{/* <FontAwesomeIcon icon={faTrash} /> */}</div>
+      <div>
+        <FontAwesomeIcon icon={faTrash} />
+      </div>
     </div>
   );
 }
